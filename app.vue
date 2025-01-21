07d8fefe88e18fee66d61a8abfc04db1e47ce99b
hustlzp <hustlzp@qq.com>
2025-01-21 09:57:51 +0800

    feat: first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen flex justify-center items-center font-typewriter text-4xl text-center">The Keyboard Company</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'The Keyboard Company',
+})
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [{ rel: "icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⌨️</text></svg>" }]
+    }
+  }
+})
